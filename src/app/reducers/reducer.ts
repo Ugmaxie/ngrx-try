5788866a88aeb694b93eeac712b733c846049742
ugi <ugmaxie@gmail.com>
2017-05-26 15:47:18 +0300

    fix(reducer): fix reducer error reset

--- src/app/reducers/reducer.ts
+++ src/app/reducers/reducer.ts
@@ -21,7 +21,7 @@ export function myWildReducer(state = defaultState, action: Action): TodoState {
     case TodoActions.ADD_NEW_GUEST:
     case TodoActions.REMOVE_GUEST:
 
-      return Object.assign({}, state, {pendingResponse: true});
+      return Object.assign({}, state, {error: null, pendingResponse: true});
 
     case TodoActions.GET_TITLE_SUCCESS:
     case TodoActions.SET_NEW_TITLE_SUCCESS:
@@ -29,7 +29,7 @@ export function myWildReducer(state = defaultState, action: Action): TodoState {
     case TodoActions.ADD_NEW_GUEST_SUCCESS:
     case TodoActions.REMOVE_GUEST_SUCCESS:
 
-      return Object.assign({}, state, action.payload, {pendingResponse: false});
+      return Object.assign({}, state, action.payload, {error: null, pendingResponse: false});
 
     case TodoActions.GET_TITLE_ERROR:
     case TodoActions.SET_NEW_TITLE_ERROR:
@@ -37,7 +37,7 @@ export function myWildReducer(state = defaultState, action: Action): TodoState {
     case TodoActions.ADD_NEW_GUEST_ERROR:
     case TodoActions.REMOVE_GUEST_ERROR:
 
-      return Object.assign({}, state, {error: action.payload}, {pendingResponse: false});
+      return Object.assign({}, state, {error: action.payload, pendingResponse: false});
 
     default:
       return state;
